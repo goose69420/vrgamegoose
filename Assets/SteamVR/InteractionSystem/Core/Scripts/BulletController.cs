9cd380c5b7f476fca0385d40c9f3710b2d7c5829
goose69420 <80535225+goose69420@users.noreply.github.com>
2021-05-11 19:11:45 +0400

    d

---
+++ Assets/SteamVR/InteractionSystem/Core/Scripts/BulletController.cs
@@ -0,0 +1,48 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+//
+// Purpose: Basic throwable object
+//
+//=============================================================================
+
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem
+{
+    public class BulletController : MonoBehaviour
+    {
+        public long speed;
+        CharacterController controller;
+        public GameObject parent;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 transform1 = transform.position;
+            if (transform1.x < 0 || transform1.y < 0 || transform1.z < 0)
+            {
+                Destroy(gameObject);
+            }
+            if (transform1.x > 1000 || transform1.y > 1000 || transform1.z > 1000)
+            {
+                Destroy(gameObject);
+            }
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log(other.gameObject.name);
+            if ((parent != null && other.gameObject == parent)) return;
+            if (other.CompareTag("Bullet")) return;
+
+            
+
+            Destroy(gameObject);
+        }
+    }
+}
